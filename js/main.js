cdff47fe8786cfc193118b5977eacf0ebd35ee0b
«Marina» <your_email@whatever.com>
2020-02-09 23:14:08 +0300

    Личный проект: больше деталей (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,47 +1,70 @@
 'use strict';
 
-//Объявила переменные
-var PRICE = ['any price', '10000 - 50000', 'less 10000', 'more 50000'];
-var TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var ROOMS = ['any rooms', '1 room', '2 rooms', '3 rooms'];
-var GUESTS = ['any guests', '2 guests', '1 guest', '0 guests'];
+var TYPE = ['Дворец', 'Квартира', 'Дом', 'Бунгало'];
 var TIME = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var FEATURES = ['WiFi', 'Стиральная машина', 'Парковка', 'Кухня', 'Лифт', 'Кондиционер'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var MAP_WIDTH = 1200;
+var LOCATION_Y_MIN = 130;
+var LOCATION_Y_MAX = 630;
+var OFFERS_COUNT = 8;
 
-//Функция, генерирующая число от 01 до 08
-var getNumberAddZero = function() {
   return "0" + Math.floor(Math.random() * 8) + 1;
-}
-
-//Функция, выбирающая случайный элемент массива
 function arrayRandElement(arr) {
     var rand = Math.floor(Math.random() * arr.length);
     return arr[rand];
 }
 
-//Функция для создания массива из 8 сгенерированных JS объектов
-var AdvertisementDescription = function() {
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var generateOffers = function(count) {
+  var countOffers = getRandomIntInclusive(FEATURES.length)
+  var feature = arrayRandElement(FEATURES);
+
+  for (var i = 0; i < count; i++) {
+    while (features.includes(feature)) {
+      feature = arrayRandElement(FEATURES);
+    }
+    return result;
+  }
+}
+
+coordX = getRandomNumber(0, 1200);
+coordY = getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX);
+
+var createOffer = function() {
   return {
     'author': {
-      'avatar': 'img/avatars/user' + getNumberAddZero + '.png';
+      'avatar': 'img/avatars/user' + '0' + getRandomNumber(1, 8) + '.png',
     };
     'offer': {
-      'title': 'Милая, уютная квартирка в центре Токио';
-      'address': '600, 350';
-      'price': arrayRandElement(PRICE);
-      'type': arrayRandElement(TYPE);
-      'rooms': arrayRandElement(ROOMS);
-      'guests': arrayRandElement(GUESTS);
-      'checkin': arrayRandElement(TIME);
-      'checkout': arrayRandElement(TIME);
-      'features': arrayRandElement(FEATURES);
-      'description': 'Описание';
-      'photos': arrayRandElement(PHOTOS);
+      'title': 'Милая, уютная квартирка в центре Токио' + (i + 1),
+      'address': 'coordX' + ',' + 'coordY',
+      'price': getRandomNumber(1, 1000000),
+      'type': arrayRandElement(TYPE),
+      'rooms': getRandomNumber(1, 100),
+      'guests': getRandomNumber(1, 3),
+      'checkin': arrayRandElement(TIME),
+      'checkout': arrayRandElement(TIME),
+      'features': arrayRandElement(FEATURES),
+      'description': 'Описание' + (i + 1),
+      'photos': arrayRandElement(PHOTOS),
     };
     'location': {
-      'x': //???случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-      'y': //???случайное число, координата y метки на карте от 130 до 630.
+      'x': coordX,
+      'y': coordY,
     };
   };
+};
+
+var getListOfOffers = function () {
+  var offers = [];
+  for (var i = 0; i < OFFERS_COUNT; i++) {
+    offers[i] = createOffer(i);
+  }
+  return offers;
 };
